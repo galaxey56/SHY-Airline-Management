e1ec77252538c69c54cd6917f9d5e08b0553d875
Galaxy <satyakiran56@gmail.com>
2021-11-18 13:17:48 +0530

    added update function

--- jdbc/src/SQLQueries/passengerSQL.java
+++ jdbc/src/SQLQueries/passengerSQL.java
@@ -1,4 +1,5 @@
 package SQLQueries;
+
 import JDBC.ConnectionEst;
 import PassengersList.Passenger;
 import java.sql.Connection;
@@ -7,19 +8,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class passengerSQL {
-    public static Passenger createObject(String args[]) throws SQLException{
-        if(args.length != 6){
+    public static Passenger createObject(String args[]) throws SQLException {
+        if (args.length != 6) {
             System.out.println("Please enter all required values in defined order");
         }
         Passenger passenger = new Passenger(args[1], args[2], Integer.parseInt(args[3]), args[4], args[5]);
-        passenger.setId(getTottalNumOfPassengers()+1);
-        long ticket = (Math.round(Math.random()*10000));
+        passenger.setId(getTottalNumOfPassengers() + 1);
+        long ticket = (Math.round(Math.random() * 10000));
         passenger.setTicketNum(passenger.getId() + "-" + ticket);
         System.out.println(passenger.toString());
         insertIntoSQL(passenger);
         return passenger;
     }
-    public static void insertIntoSQL(Passenger p) throws SQLException{
+
+    public static void insertIntoSQL(Passenger p) throws SQLException {
         Connection need = ConnectionEst.establishConnection();
         String query = "insert into passenger values(?,?,?,?,?,?,?)";
         PreparedStatement executableQuery = need.prepareStatement(query);
@@ -32,37 +34,75 @@ public class passengerSQL {
         executableQuery.setString(7, p.getTicketNum());
         executableQuery.executeUpdate();
     }
-    public static int getTottalNumOfPassengers() throws SQLException{
+
+    public static int getTottalNumOfPassengers() throws SQLException {
         Connection need = ConnectionEst.establishConnection();
         String query = "select count(*) from passenger";
         PreparedStatement executableQuery = need.prepareStatement(query);
         ResultSet ans = executableQuery.executeQuery();
         return ans.getInt(1);
     }
-    public static void searchWithName(String name) throws SQLException{
+
+    public static void searchWithName(String name) throws SQLException {
         Connection need = ConnectionEst.establishConnection();
         String query = "select * from passenger where name = ?";
         PreparedStatement executableQuery = need.prepareStatement(query);
         executableQuery.setString(1, name);
         ResultSet ans = executableQuery.executeQuery();
-        if(!ans.next()){
+        if (!ans.next()) {
             System.out.println("No Match Found");
             return;
         }
         Passenger p = Passenger.makePassenger(ans);
         System.out.println(p.toString());
     }
-    public static void searchWithId(int id) throws SQLException{
+
+    public static void searchWithId(int id) throws SQLException {
         Connection need = ConnectionEst.establishConnection();
         String query = "select * from passenger where id = ?";
         PreparedStatement executableQuery = need.prepareStatement(query);
         executableQuery.setInt(1, id);
         ResultSet ans = executableQuery.executeQuery();
-        if(!ans.next()){
+        if (!ans.next()) {
             System.out.println("No Match Found");
             return;
         }
         Passenger p = Passenger.makePassenger(ans);
         System.out.println(p.toString());
+    }
+
+    public static void update(String args[]) throws SQLException {
+        int id = Integer.parseInt(args[3]);
+        switch (args[2]) {
+        case "-n":
+            updateName(id, args[4]);
+            break;
+        case "-a":
+            updateAge(id, Integer.parseInt(args[4]));
+            break;
+        case "-g":
+            updateGender(id, args[4]);
+            break;
+        case "-p":
+            updateMobileNum(id, args[4]);
+            break;
+        case "-e":
+            updateEmail(id, args[4]);
+        }
+    }
+    private static void updateName(int id, String name){
+        
+    }
+    private static void updateAge(int id, int age){
+
+    }
+    private static void updateGender(int id, String gender){
+
+    }
+    private static void updateMobileNum(int id, String num){
+
+    }
+    private static void updateEmail(int id, String email){
+        
     }
 }
